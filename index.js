1f21af0d8ac3fa2982dc9f7f8ffb0fd4f830c532
Ajitesh Tiwari <ajitesh.tiwari@mrcooper.com>
2018-11-22 02:17:40 +0530

    Making web-call and parsing the result

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const program = require('commander');
+const axios = require('axios');
+const jsdom = require("jsdom");
+
+const {JSDOM} = jsdom;
 
 program
     .version('1.0.0')
     .description('A command-line utility for information on file extension.');
 
 program
-    .command('exxt')
+    .command('exxt <extension>')
     .description('A command-line utility for information on file extension.')
-    .action(() => {
-        console.log('Hello')
+    .action((extension) => {
+
+        axios.get('https://fileinfo.com/extension/' + extension)
+            .then(function (response) {
+                console.log(getElementByXpath(response.data, '//*[@id="left"]/article/h1/span'));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     });
+
+
+function getElementByXpath(document, path) {
+    const window = new JSDOM(document.body).window;
+    return window.document.evaluate(path, window.document.documentElement, null, window.XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent;
+}
+
+program.parse(process.argv);
